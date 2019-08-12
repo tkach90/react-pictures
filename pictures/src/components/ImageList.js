ac2a74b45d0e1837f26e1c69ee6d6fc356264bba
vlad.tkach <vlad.tkach@asmbrain.com>
2019-08-12 17:51:49 +0300

    ImageList start to implement css grid to ImageList component

--- pictures/src/components/ImageList.js
+++ pictures/src/components/ImageList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import './ImageList.css';
+
 const ImageList = props => {
     const images = props.images.map(({id, urls, description}) => {
        return <img key={id} src={urls.regular} alt={description} />
     });
 
-    return <div>{images}</div>;
+    return <div className='image-list'>{images}</div>;
 };
 
 export default ImageList;
